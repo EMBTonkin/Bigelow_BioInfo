b7f08ceec4d6561ed4099dc352db00c4e94695f6
Lizzie Tonkin <ltonkin@bigelow.org>
2014-07-23 15:31:20 -0400

    First real commit
    Added the fastq file, along with a samller version for testing.  Created MultiProsses.py, which so far, groups the four lines in the fastq file which go together, and then sends them out to be multi prossesed.

---
+++ MultiProsses.py
@@ -0,0 +1,27 @@
+
+ 
+import multiprocessing
+from itertools import izip_longest
+ 
+def process_chunk(d):
+	return d[0]+ "Lizzie is cool"
+ 
+def grouper(iterable, n, fillvalue=None):
+    # "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+ 
+if __name__ == '__main__':
+ 
+	
+	test_data = file("test.fastq", 'rU').readlines()
+	
+	p = multiprocessing.Pool(4)
+ 	
+	chunks = []	
+	for chunk in grouper(test_data, 4):
+		chunks.append(chunk)
+	
+	results = p.map(process_chunk, chunks)
+	print results
