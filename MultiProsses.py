1f1e7c5c4a07eae337077935daa2043c93bc0826
Lizzie Tonkin <ltonkin@bigelow.org>
2014-07-24 11:21:47 -0400

    Add comments
    I started commenting things, because it is good practice.

--- MultiProsses.py
+++ MultiProsses.py
@@ -1,22 +1,37 @@
+# Lizzie Tonkin
+# Multiprocessing processing of fastq files
+# Bigelow Internship, 2014
+
+
+# Important Import Stuff
 import multiprocessing
 from itertools import izip_longest
 
+
+# does not do anything useful now, but soon it will do useful things to the records and return those useful things
 def process_chunk(chunk):
-	#Just a silly function which proves this is doing something by taking the name of the sequence and adding a know fact to it,
 	return chunk[0]+ "Lizzie is cool"
 
+# Groups the input into hunks of four
+# returns a tuple
+# code directly copied off the python site
 def grouper(iterable, n, fillvalue=None):
     # "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return izip_longest(fillvalue=fillvalue, *args)
 
+# Creates a generator which returns a list containing the three useful lines per fastq record
+# filename: string, name of the fastq file
+# returns generator, which yields three element lists containing the important parts of each fastq record.
 def makeFASTQGenerator(filename):
 	data =  file(filename, 'rU').readlines()
 	for chunk in grouper(data, 4):
 		chunky = [chunk[0].strip("\n"),chunk[1].strip("\n"),chunk[3].strip("\n")]
 		yield chunky
 
+
+# Maine function
 if __name__ == '__main__':
 	
 	p = multiprocessing.Pool(4)
