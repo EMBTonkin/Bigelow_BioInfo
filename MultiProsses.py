153aad64747ace33c5368fa9d7146bb1a5e4b19b
Lizzie Tonkin <ltonkin@bigelow.org>
2014-07-24 11:07:36 -0400

    Add generator to produce individual fastq records
    I have greated an oh-so-fancy generator which iterates through the fastq file and returns chunks containing the three useful lines of information.

--- MultiProsses.py
+++ MultiProsses.py
@@ -1,25 +1,32 @@
 import multiprocessing
 from itertools import izip_longest
- 
-def process_chunk(d):
+
+def process_chunk(chunk):
 	#Just a silly function which proves this is doing something by taking the name of the sequence and adding a know fact to it,
-	return d[0]+ "Lizzie is cool"
- 
+	return chunk[0]+ "Lizzie is cool"
+
 def grouper(iterable, n, fillvalue=None):
     # "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
     args = [iter(iterable)] * n
     return izip_longest(fillvalue=fillvalue, *args)
- 
+
+def makeFASTQGenerator(filename):
+	data =  file(filename, 'rU').readlines()
+	for chunk in grouper(data, 4):
+		chunky = [chunk[0].strip("\n"),chunk[1].strip("\n"),chunk[3].strip("\n")]
+		yield chunky
+
 if __name__ == '__main__':
- 
-	test_data = file("test.fastq", 'rU').readlines()
 	
 	p = multiprocessing.Pool(4)
- 	
-	chunks = []	
-	for chunk in grouper(test_data, 4):
-		chunks.append(chunk)
 	
-	results = p.map(process_chunk, chunks)
-	print results
+	for name, seq, qual in makeFASTQGenerator("test.fastq"):
+		print name
+		print seq
+		print qual
+		print ""
+	
+#	results = p.map(process_chunk, makeFASTQGenerator("test.fastq"))
+#	print results
+
