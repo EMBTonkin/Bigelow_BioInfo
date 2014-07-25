4664202774fc924c307dab1d6b1a049a8ec35c59
Lizzie Tonkin <ltonkin@bigelow.org>
2014-07-25 14:12:46 -0400

    Add GC analysis, better file reading, better comments
    The files are parsed by a better function by Joe, but I have very little idea how.  Quite hard to read.  But I tested it, and it works.
    The analysis function now computes the CG content of the DNA.
    I made better docstrings for the functions, to comply with PEP8, which I had never heard of before now, but I will now try hard to comply with it.

---
+++ README.md
@@ -0,0 +1,9 @@
+You read me.
+Congratulations.
+Was it worth it?
+
+
+
+
+
+Eventually I'll put useful information in this file I suppose, but until there, there's not much of a point.
--- multiprocess.py
+++ multiprocess.py
@@ -7,48 +7,70 @@ Bigelow Internship, 2014
 """
 
 # Important Import Stuff
-import sys
+import argparse
 import multiprocessing
 from itertools import islice, izip_longest
 
 
 def read_fastq(fq):
-    # add gzip support here
-    with open(fq) as fh:
-        #
-        fqclean = (x.strip("\r\n") for x in fh if x.strip())
-        while True:
-            #
-            r = [x for x in islice(fqclean, 4)]
-            #
-            if not r: raise StopIteration
-            #
-            assert all(r) and len(r) == 4
-            #
-            yield r[0][1:], r[1], r[3]
+	# add gzip support here
+	with open(fq) as fh:
+		# Somehow removes blank lines from the file
+		fqclean = (x.strip("\r\n") for x in fh if x.strip())
+		while True:
+			# a generator which divides the lines into groups of four
+			r = [x for x in islice(fqclean, 4)]
+			# if we have gotten to the end, stop
+			if not r: raise StopIteration
+			# Insists that records be four lines long somehow
+			assert all(r) and len(r) == 4
+			# yield fist, second and fourth line, where the first character of the fist line has been removed.
+			yield r[0][1:], r[1], r[3]
 
 
-# does not do anything useful now, but soon it will do useful things to the records and return those useful things
-def process_chunk(chunk):
-	return chunk[0]+ "Lizzie is cool"
+def processChunk(chunk):
+	"""Process the information in each fastq record by doing the following:
+	Calculate GC content as a percent of total observed sequence.
+	Results are printed right now, but will be put in a nicely formatted file at a later date.
+	"""
+	
+	name,sequence,char=chunk
+	
+	# caluculate the CG %
+	gc = 0.0
+	for char in sequence:
+		if (char == "C") or (char == "G"):
+			gc += 1
+	gcContent = gc / (len(sequence))
+	
+	#print out the lovely results in a mildly understandable way
+	print name
+	print "GC content is "+ str(gcContent)
+	print
 
-# Groups the input into hunks of four
-# returns a tuple
-# code directly copied off the python site
+
 def grouper(iterable, n, fillvalue=None):
-    # "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
-    args = [iter(iterable)] * n
-    return izip_longest(fillvalue=fillvalue, *args)
+	"""Group the input into hunks of four.
+	Return is a tuple containing the four things.
+	This code is directly copied off the python site.
+	"""
+	# "Collect data into fixed-length chunks or blocks"
+	# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+	args = [iter(iterable)] * n
+	return izip_longest(fillvalue=fillvalue, *args)
 
-# Creates a generator which returns a list containing the three useful lines per fastq record
-# filename: string, name of the fastq file
-# returns generator, which yields three element lists containing the important parts of each fastq record.
+
 def makeFASTQGenerator(filename):
+	"""Create a generator which returns a list containing the three useful lines per fastq record.
+	If you have sporadic empty lines in your file, you don't deserve to have this work for you; learn to be consistent.
+	filename is a string which is the name of the fastq file.
+	Return a generator, which yields lists containing the three important parts of each fastq record per list.
+	
+	Mildly useless, since Joe made a better version
+	"""
 	with open(filename, 'rU') as f:
 		for i in range(5):
 			fifth = f.readline()
-        # empty lines may be sporadic, so this will fail
 		if fifth == "\n":
 			f.seek(0)
 			for chunk in grouper(f.readlines(), 5):
@@ -62,57 +84,53 @@ def makeFASTQGenerator(filename):
 
 
 def validateRecords(filename):
-    """as an example, docstrings go here.
-    validates records by making sure all three parts of the record exist
-    filename: string, name of the fastq file
-    returns boolean: true if the records in the file are valid, false of not.
-    """
+	"""Validate all records in a file by making sure all three parts of the record exist.
+	filename is a string which is the name of the fastq file.
+	Return a boolean, which is true if the records in the file are valid, false of not.
+	"""
 	for record in makeFASTQGenerator(filename):
 		for part in record:
 			if part.isspace() or (part == ""):
 				return False
 	return True
-
-
+	
+	
 def main(fastq):
-    # for name, seq, ... etc
-
-    # calculate GC content as a percent of total observed sequence.
-    # you need to know how many bases were present (len(sequence))
-    # for each sequence, you need to count 'G' and 'C'
-    # you don't have to worry about doing anything in multiprocessing yet
+	"""Future home of awesome multiprocessing"""
+	
+#	if not validateRecords(fastq):
+#		print "Check your input is formatted nicely"
+#		quit()
+	
+	for record in read_fastq(fastq):
+		processChunk(record)
 
 
 # Maine function
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument('test')
-    args = p.parse_args()
-    main(args.test)
-
-	# check for proper amount of command line arguments
-    # if len(sys.argv) < 2:
-    #     print ''
-    #     print 'Oi!  Inproper file usage!'
-    #     print 'usage: python MultiProsses.py <filename.fastq>'
-    #     print ''
-    #     quit()
-    #
-    # # check for proper record formatting
-    # if validateRecords(sys.argv[1]) == False:
-    #     print ''
-    #     print 'Error!  Invalid input!'
-    #     print 'Something in your record is invalid.  Make sure that all parts of the record are there and formated consistently'
-    #     print ''
-    #     quit()
-    #
-    # p = multiprocessing.Pool(4)
-    #
-    # for name, seq, qual in makeFASTQGenerator(sys.argv[1]):
-    #     print name
-    #     print seq
-    #     print qual
-    #     print ""
-
-#	results = p.map(process_chunk, makeFASTQGenerator("test.fastq"))
-#	print results
+	p = argparse.ArgumentParser(description=__doc__)
+	p.add_argument('test')
+	args = p.parse_args()
+	main(args.test)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
